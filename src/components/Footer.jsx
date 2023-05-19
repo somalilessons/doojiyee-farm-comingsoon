70042b6337cb38996898e5e8d25a2bfa04c9dc70
somalilessons <somalilessons5@gmail.com>
2023-05-19 07:32:16 +0300

    initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="container">
+        <a href="#">Doojiyee Farms </a>.
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
